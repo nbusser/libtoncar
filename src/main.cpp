4adce238864cd97a68a66d6e6ed40b6bd3d01251
Nicolas Busser <nicolas.busser67@gmail.com>
2026-02-23 00:06:49 +0900

    pick libtonc

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
-#include <gba_console.h>
-#include <gba_interrupt.h>
-#include <gba_systemcalls.h>
+// Copyright 2022 Dietrich Epp.
+// This file is part of Bloodlight. Bloodlight is licensed under the terms of
+// the Mozilla Public License, version 2.0. See LICENSE.txt for details.
 
-#include <cstdio>
-#include <cstdlib>
+#include <stdlib.h>
+#include <tonc.h>
 
-int main() {
-  irqInit();
-  irqEnable(IRQ_VBLANK);
-
-  consoleDemoInit();
-
-  printf("\x1b[10;10HHello From Bazel!\n");
-
-  while (true) {
+int main(void) {
+  while (1) {
     VBlankIntrWait();
   }
 }
