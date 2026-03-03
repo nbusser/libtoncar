6b31536db3e303bd1b25b13c7c540078964c155d
Nicolas Busser <nicolas.busser67@gmail.com>
2026-03-03 21:28:14 +0900

    add CLI argument parsing

--- libtoncar/gfx_converter/src/main.rs
+++ libtoncar/gfx_converter/src/main.rs
@@ -1,8 +1,20 @@
-use std::path::Path;
-
 use asefile::AsepriteFile;
+use std::{env, path::Path};
+
+fn convert_ase(filepath: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let _ase = AsepriteFile::read_file(filepath)?;
+    Ok(())
+}
 
 fn main() {
-    let filename = Path::new("test.ase");
-    let _ase = AsepriteFile::read_file(filename).expect("Aseprite file should exist");
+    let filename = match env::args().nth(1) {
+        Some(f) => f,
+        None => {
+            eprintln!("Usage: converter <aseprite filepath>");
+            std::process::exit(1);
+        }
+    };
+
+    let _ = convert_ase(Path::new(&filename));
+    ()
 }
