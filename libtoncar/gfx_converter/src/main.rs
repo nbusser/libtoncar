5af031764aa283fac8378507849080b6236cd47a
Nicolas Busser <nicolas.busser67@gmail.com>
2026-03-03 23:45:50 +0900

    parsing

--- libtoncar/gfx_converter/src/main.rs
+++ libtoncar/gfx_converter/src/main.rs
@@ -1,20 +1,56 @@
-use asefile::AsepriteFile;
-use std::{env, path::Path};
+use asefile::{AsepriteFile, Tag};
+use image::DynamicImage;
+use std::{
+    env::{self},
+    path::Path,
+};
 
-fn convert_ase(filepath: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let _ase = AsepriteFile::read_file(filepath)?;
-    Ok(())
+fn parse_aseprite_file(
+    filepath: &Path,
+) -> Result<(Vec<DynamicImage>, Vec<Tag>), Box<dyn std::error::Error>> {
+    let ase = AsepriteFile::read_file(filepath)?;
+
+    let mut images = Vec::new();
+    let mut tags = Vec::new();
+
+    for frame in 0..ase.num_frames() {
+        let image = ase.frame(frame).image();
+
+        images.push(DynamicImage::ImageRgba8(image))
+    }
+
+    for tag in 0..ase.num_tags() {
+        tags.push(ase.tag(tag).clone())
+    }
+
+    Ok((images, tags))
 }
 
 fn main() {
-    let filename = match env::args().nth(1) {
-        Some(f) => f,
-        None => {
-            eprintln!("Usage: converter <aseprite filepath>");
-            std::process::exit(1);
-        }
-    };
+    if env::args().len() < 2 {
+        eprintln!("Usage: converter <aseprite filepaths>");
+        std::process::exit(1);
+    }
 
-    let _ = convert_ase(Path::new(&filename));
+    let parsed_aseprite_files: Vec<(
+        String,
+        Result<(Vec<DynamicImage>, Vec<Tag>), Box<dyn std::error::Error>>,
+    )> = env::args()
+        .skip(1)
+        .map(|filepath| (filepath.clone(), parse_aseprite_file(Path::new(&filepath))))
+        .collect();
+
+    let (_parsed, errs): (Vec<_>, Vec<_>) = parsed_aseprite_files
+        .into_iter()
+        .partition(|(_, res)| res.is_ok());
+
+    if errs.len() > 0 {
+        eprintln!("Parsing errors:");
+        errs.iter().for_each(|(filepath, error)| {
+            eprintln!("File {}: \"{}\"", filepath, error.as_ref().unwrap_err());
+        });
+        std::process::exit(1);
+    }
+
     ()
 }
